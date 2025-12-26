ad5d5279be55db90001271674ef94fbf486043f2
asaiie <amit_sai_erraguntla@brown.edu>
2025-12-26 12:39:50 -0500

    normal vector

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,27 @@ use std::io::{BufWriter, Write};
 
 use crate::vec3::Point3;
 
+fn hit_sphere(center: Point3, radius: f64, r: Ray) -> f64 {
+    let oc = center - r.origin();
+    let a = Vec3::dot(&r.direction(), &r.direction());
+    let b = -2.0 * Vec3::dot(&r.direction(), &oc);
+    let c = Vec3::dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, *r);
+    if t > 0.0 {
+        let N = Vec3::unit_vector(&(r.at(t) - Vec3::new(0.0, 0.0, -1.0)));
+        return 0.5 * Color::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
+    }
+
     let unit_direction = Vec3::unit_vector(&r.direction());
     let a = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
