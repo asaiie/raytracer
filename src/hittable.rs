efe2e75f5e052a6ba21bb93932bc66888c633886
asaiie <amit_sai_erraguntla@brown.edu>
2025-12-28 10:34:10 -0500

    normal vector

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,10 +1,24 @@
-use crate::{vec3::{Point3, Vec3}};
-use crate::{ray::Ray};
+use crate::ray::Ray;
+use crate::vec3::{Point3, Vec3};
 
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
     pub t: f64,
+    pub front_face: bool,
+}
+
+impl HitRecord {
+    pub fn set_face_normal(&mut self, r: &Ray, outward_normal: &Vec3) {
+        // Sets the hit record normal vector
+        // NOTE: the param `outward_normal` is assumed to have unit length
+        self.front_face = r.direction().dot(outward_normal) < 0.0;
+        self.normal = if self.front_face {
+            *outward_normal
+        } else {
+            -*outward_normal
+        }
+    }
 }
 
 pub trait Hittable {
--- src/sphere.rs
+++ src/sphere.rs
@@ -1,4 +1,8 @@
-use crate::{hittable::{HitRecord, Hittable}, ray::Ray, vec3::Point3};
+use crate::{
+    hittable::{HitRecord, Hittable},
+    ray::Ray,
+    vec3::Point3,
+};
 
 pub struct Sphere {
     center: Point3,
@@ -7,7 +11,10 @@ pub struct Sphere {
 
 impl Sphere {
     pub fn new(center: Point3, radius: f64) -> Self {
-        Sphere { center, radius: radius.max(0.0) }
+        Sphere {
+            center,
+            radius: radius.max(0.0),
+        }
     }
 }
 
@@ -18,7 +25,7 @@ impl Hittable for Sphere {
         let h = r.direction().dot(&oc);
         let c = oc.length_squared() - self.radius * self.radius;
 
-        let discriminant= h * h - a * c;
+        let discriminant = h * h - a * c;
         if discriminant < 0.0 {
             return false;
         }
@@ -36,7 +43,8 @@ impl Hittable for Sphere {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - self.center) / self.radius;
+        let outward_normal = (rec.p - self.center) / self.radius;
+        rec.set_face_normal(r, &outward_normal);
 
         true
     }
