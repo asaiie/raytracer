dedb072c8ccdd7756649e155894dd380a0e45df2
asaiie <amit_sai_erraguntla@brown.edu>
2025-12-31 18:41:38 -0500

    final scene

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ mod sphere;
 mod utils;
 mod vec3;
 
+use rand::Rng;
+
 use crate::camera::Camera;
 use crate::color::Color;
 use crate::hittable_list::HittableList;
@@ -18,64 +20,90 @@ use crate::vec3::{Point3, Vec3};
 
 fn main() -> std::io::Result<()> {
     let mut world = HittableList::new();
+    let mut rng = rand::thread_rng();
 
-    let material_ground = Material::Lambertian {
-        albedo: Color::new(0.8, 0.8, 0.0),
+    let ground_material = Material::Lambertian {
+        albedo: Color::new(0.5, 0.5, 0.5),
     };
-    let material_center = Material::Lambertian {
-        albedo: Color::new(0.1, 0.2, 0.5),
-    };
-    let material_left = Material::Dielectric {
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new(
+                a as f64 + 0.9 * rng.gen::<f64>(),
+                0.2,
+                b as f64 + 0.9 * rng.gen::<f64>(),
+            );
+
+            if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                let sphere_material = if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random(0.0, 1.0) * Color::random(0.0, 1.0);
+                    Material::Lambertian { albedo }
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random(0.5, 1.0);
+                    let fuzz = rng.gen_range(0.0..0.5);
+                    Material::Metal { albedo, fuzz }
+                } else {
+                    // glass
+                    Material::Dielectric {
+                        refraction_index: 1.5,
+                    }
+                };
+
+                world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+            }
+        }
+    }
+
+    let material1 = Material::Dielectric {
         refraction_index: 1.5,
     };
-    let material_bubble = Material::Dielectric {
-        refraction_index: 1.0 / 1.5,
-    };
-    let material_right = Material::Metal {
-        albedo: Color::new(0.8, 0.6, 0.2),
-        fuzz: 1.0,
-    };
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
 
+    let material2 = Material::Lambertian {
+        albedo: Color::new(0.4, 0.2, 0.1),
+    };
     world.add(Box::new(Sphere::new(
-        Point3::new(0.0, -100.5, -1.0),
-        100.0,
-        material_ground,
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
     )));
+
+    let material3 = Material::Metal {
+        albedo: Color::new(0.7, 0.6, 0.5),
+        fuzz: 0.0,
+    };
     world.add(Box::new(Sphere::new(
-        Point3::new(0.0, 0.0, -1.2),
-        0.5,
-        material_center,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.5,
-        material_left,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(-1.0, 0.0, -1.0),
-        0.4,
-        material_bubble,
-    )));
-    world.add(Box::new(Sphere::new(
-        Point3::new(1.0, 0.0, -1.0),
-        0.5,
-        material_right,
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
     )));
 
     let mut cam = Camera::new();
 
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 400;
-    cam.samples_per_pixel = 100;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 500;
     cam.max_depth = 50;
 
     cam.vfov = 20.0;
-    cam.lookfrom = Point3::new(-2.0, 2.0, 1.0);
-    cam.lookat = Point3::new(0.0, 0.0, -1.0);
+    cam.lookfrom = Point3::new(13.0, 2.0, 3.0);
+    cam.lookat = Point3::new(0.0, 0.0, 0.0);
     cam.vup = Vec3::new(0.0, 1.0, 0.0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist = 10.0;
 
     cam.render(&world)?;
     Ok(())
